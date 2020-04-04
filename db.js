84bf9b8ad5dffe941b662f96c2e6140c98bc640b
Selamawit Moges <sm6957@nyu.edu>
2020-04-04 07:14:06 -0400

    preliminary login done

--- db.js
+++ db.js
@@ -9,44 +9,67 @@ const mongoose = require('mongoose');
 //this will store users personal information 
 let Schema = mongoose.Schema;
 let userPersonalInfo = new mongoose.Schema({
-    firstName: 'first name',
-    lastName: 'last name',
-    userID: 'student id',
-    university: 'unversity name'
-})
+    firstName: {
+        type: String, 
+        required: true
+    },
+    lastName: {
+        type: String, 
+        required: true
+    },
+    email:{
+        type: String, 
+        required: true
+    },
+    userID:{
+        type: String, 
+        required: true
+    },
+    password:{
+        type: String, 
+        required: true
+    },
+    university: {
+        type: String, 
+        required: true
+    },
+});
 
 //this will store specific meal plan that each user has 
-let userMealPlanData = new mongoose.Schema({ 
-    userID: 'student_id',
-    mealPlanFall: 
-        { planName: "300 Flex Plus", 
-          mealPerSemester: 300, 
-          diningDollars: 250,
-          numberOfMealsDistributed: 0
-        },
-    mealPlanSpring: 
-        { planName: "300 Flex Plus", 
-          mealPerSemester: 300, 
-          diningDollars: 250,
-          numberOfMealsDistributed: 0
-        }
+
+// let userMealPlanData = new mongoose.Schema({ 
+//     userID: 'student_id',
+//     mealPlanFall: 
+//         { planName: "300 Flex Plus", 
+//           mealPerSemester: 300, 
+//           diningDollars: 250,
+//           numberOfMealsDistributed: 0
+//         },
+//     mealPlanSpring: 
+//         { planName: "300 Flex Plus", 
+//           mealPerSemester: 300, 
+//           diningDollars: 250,
+//           numberOfMealsDistributed: 0
+//         }
       
-})
+// })
 
 //how many meals a user sent to another user or to the donation pool 
-let Exchange = new mongoose.Schema({
-    userID: 'student_id', 
-   direct: {
-       studentID: 'student_id',
-       amount: 0
-   },
-   donation: {
-       amount:0
-   }
-})
 
+// let Exchange = new mongoose.Schema({
+//     userID: 'student_id', 
+//    direct: {
+//        studentID: 'student_id',
+//        amount: 0
+//    },
+//    donation: {
+//        amount:0
+//    }
+// })
 
-module.exports = {
-    userMealPlanData: userMealPlanData,
-    userPersonalInfo: userPersonalInfo
-}
+const User = module.exports = mongoose.model('userPersonalInfo', userPersonalInfo)
+mongoose.connect('mongodb://localhost/UserData',{ useNewUrlParser: true, useUnifiedTopology: true });
+// module.exports = {
+//     userMealPlanData: userMealPlanData,
+//     userPersonalInfo: userPersonalInfo
+// }
---
+++ server.js
@@ -0,0 +1,119 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  const db = require('./db.js');
+  const mongoose = require('mongoose');
+  const User = mongoose.model('userPersonalInfo');
+  mongoose.Promise = global.Promise;
+  const path = require('path');
+
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  const methodOverride = require('method-override')
+  app.use(express.static(path.join(__dirname, 'public')));
+let tester = '';
+  const initializePassport = require('./passport-config')
+  initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+   id => users.find(user => user.id === id)
+    // email =>  User.find({email: email}),
+    // id => User.find({userID: userID})
+  )
+  
+  const users = []
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.render('landing.hbs')
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.hbs')
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.hbs')
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      let fname = req.body.fname
+      let lname = req.body.lname
+      let email = req.body.email
+      let uni = req.body.university
+      let userID = req.body.userID
+      let password = hashedPassword
+
+      let newUser = {
+          'firstName': fname, 
+          'lastName': lname, 
+          'email': email, 
+          'university': uni, 
+          'userID':userID, 
+          'password': password
+      }
+      users.push({
+        id: Date.now().toString(),
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        university: req.body.university,
+        userID: req.body.userID,
+        password: hashedPassword
+      })
+      
+    const user = new User(newUser);
+    user.save((err, student) => {
+        tester = student.email
+      //console.log(student.em);
+      res.redirect('/login')
+    });
+      
+    } catch {
+      res.redirect('/register')
+    }
+  })
+  
+  app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+  
+  app.listen(3000)
